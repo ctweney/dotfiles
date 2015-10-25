7f8b69e1fc8bdad5bda00b1b29e2fc975e88f8a4
Chris Tweney <chris@media.berkeley.edu>
2015-10-25 13:42:32 -0700

    Spark path adjustment

--- source/99_chronos.sh
+++ source/99_chronos.sh
@@ -2,5 +2,7 @@
 if [ `hostname` == "chronos" ]; then
 	export SPARK_HOME=$HOME/spark-1.5.0
 	export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip:$HOME/dev/rails4-rescuetime/p3rt
+	export PYSPARK_PYTHON=python3
+	export PATH=$PATH:$SPARK_HOME/bin
 	source $HOME/.aws/env_vars
 fi
