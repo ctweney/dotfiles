3b4e14df5995949cdf3a6bd5f8e52f82f5095b3d
Chris Tweney <chris@media.berkeley.edu>
2022-10-05 08:57:21 -0700

    mirror script

---
+++ bin/mirror_mydrive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LOGFILE="/Users/chris/mirror.log"
+
+echo "`date`: starting mirror" >> $LOGFILE
+/opt/homebrew/bin/rsync --archive --executability --xattrs --quiet --delete "/Users/chris/My Drive/" /Volumes/BigBoid/GoogleDriveMirror/ >> $LOGFILE 2>&1
+echo "`date`: mirror complete" >> $LOGFILE
+echo "-----------------------" >> $LOGFILE
+
+
