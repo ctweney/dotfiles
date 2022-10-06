588d46677d2464f9b3eba3087553122136d2c92c
Chris Tweney <chris@media.berkeley.edu>
2022-10-06 08:51:42 -0700

    Verbose rsync

--- bin/mirror_mydrive.sh
+++ bin/mirror_mydrive.sh
@@ -3,7 +3,7 @@
 LOGFILE="/Users/chris/mirror.log"
 
 echo "`date`: starting mirror" >> $LOGFILE
-/opt/homebrew/bin/rsync --archive --executability --xattrs --quiet --delete "/Users/chris/My Drive/" /Volumes/BigBoid/GoogleDriveMirror/ >> $LOGFILE 2>&1
+/opt/homebrew/bin/rsync --archive --executability --xattrs -v --delete "/Users/chris/My Drive/" /Volumes/BigBoid/GoogleDriveMirror/ >> $LOGFILE 2>&1
 echo "`date`: mirror complete" >> $LOGFILE
 echo "-----------------------" >> $LOGFILE
 
